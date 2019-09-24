bcb5e7d14e3f275d0ee932fcbf2fbb0b2a235368
Tarek <tfzein@gmail.com>
2019-09-24 11:24:48 +0200

    All links according to Task1 are working

---
+++ EnvironmentCrime/Controllers/CoordinatorController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EnvironmentCrime.Controllers
+{
+    public class CoordinatorController : Controller
+    {
+        // GET: /<controller>/
+        public ViewResult StartCoordinator()
+        {
+            return View();
+        }
+
+        public ViewResult ReportCrime()
+        {
+            return View();
+        }
+
+        public ViewResult CrimeCoordinator()
+        {
+            return View();
+        }
+
+        public ViewResult Validate()
+        {
+            return View();
+        }
+    }
+}
---
+++ EnvironmentCrime/Controllers/InvestigatorController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EnvironmentCrime.Controllers
+{
+    public class InvestigatorController : Controller
+    {
+        // GET: /<controller>/
+        public ViewResult StartInvestigator()
+        {
+            return View();
+        }
+
+        public ViewResult CrimeInvestigator()
+        {
+            return View();
+        }
+    }
+}
